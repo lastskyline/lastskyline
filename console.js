6cecf12c1f1619bbcbd16b40ec9bbb75c80f6835
lastskyline <112888975+lastskyline@users.noreply.github.com>
2022-09-09 08:55:37 +0900

    Update console.js

--- console.js
+++ console.js
@@ -1,1 +1,3 @@
 console.log(10392736);
+alert(document.querySelector("#container > section > article:nth-child(3) > div.view_content_wrap > div > div.appending_file_box"));
+alert(parent.document.querySelector("#container > section > article:nth-child(3) > div.view_content_wrap > div > div.appending_file_box"));
