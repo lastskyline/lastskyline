ca17ce287fc7196fa075b85c5a0bff35b26e45e2
lastskyline <112888975+lastskyline@users.noreply.github.com>
2022-09-09 09:03:23 +0900

    Update console.js

--- console.js
+++ console.js
@@ -1,5 +1,4 @@
 document.domain='dcinside.com';
 console.log(10392736);
 alert(document.querySelector("#container > section > article:nth-child(3) > div.view_content_wrap > div > div.appending_file_box"));
-alert(parent.document.querySelector("#container > section > article:nth-child(3) > div.view_content_wrap > div > div.appending_file_box"));
 alert(opener.document.querySelector("#container > section > article:nth-child(3) > div.view_content_wrap > div > div.appending_file_box"));
